5541de6bf044b036272ad4619d9cee1a23579da7
ashwin-inquisitor <ashwinkr019@gmail.com>
2025-09-12 02:44:47 +0530

    code written on 125.py to print hollow inverted pyramid using loop.

---
+++ 6.Loop & Iteration Exercise/Star Pattern/125.py
@@ -0,0 +1,14 @@
+'''Print hollow inverted pyramid star pattern using loop.'''
+
+N = int(input("Enter number of rows for hollow inverted pyramind: "))
+
+for i in range(N, 0, -1):
+    for j in range(N - i):
+        print(" ", end=" ")
+
+    for k in range(1, 2 * i):
+        if k == 1 or k == 2 * i - 1 or i == N:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
