1f788f934f180edf32f74d9a6e2f1fbbfd2f63e5
ashwin-inquisitor <ashwinkr019@gmail.com>
2025-10-08 11:20:41 +0530

    code written on 207.py to find first occurrence of a character in a given string.

---
+++ 10.String Exercise/207.py
@@ -0,0 +1,11 @@
+'''Find first occurrence of a character in a given string.'''
+
+str = input("Enter string: ")
+char = input("Enter character to find: ")
+
+index = str.find(char)
+
+if index != 1:
+    print(f"First occurrence of character '{char}' is at index {index}")
+else:
+    print(f"Character '{char}' not found in the string. ")
