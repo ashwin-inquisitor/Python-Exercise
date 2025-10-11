8fd49d2d2c2fce16fe5dfac5e6d7ffbe4c5b24ef
ashwin-inquisitor <ashwinkr019@gmail.com>
2025-10-11 08:16:41 +0530

    code written on 229.py to trim trailing white space characters from given string.

---
+++ 10.String Exercise/229.py
@@ -0,0 +1,5 @@
+'''Trim trailing white space characters from given string.'''
+
+str = input("Enter string: ")
+trimmed_str = str.rstrip()
+print(f"String after trimming trailing spaces: {trimmed_str}")
