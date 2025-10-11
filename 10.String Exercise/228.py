bc0f24f721003fb7614ead0500ba04b8108b890e
ashwin-inquisitor <ashwinkr019@gmail.com>
2025-10-11 08:13:11 +0530

    code written on 228.py to trim leading white space characters from given string.

---
+++ 10.String Exercise/228.py
@@ -0,0 +1,5 @@
+'''Trim leading white space characters from given string.'''
+
+str = input("Enter string: ")
+trimmed_str = str.lstrip()
+print(f"String after trimming leading spaces: {trimmed_str}")
