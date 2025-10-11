4894c82aec427a28131433dd17be1511f722d159
ashwin-inquisitor <ashwinkr019@gmail.com>
2025-10-11 08:19:34 +0530

    code written on 230.py to trim both leading and trailing white space characters from given string.

---
+++ 10.String Exercise/230.py
@@ -0,0 +1,5 @@
+'''Trim both leading and trailing white space characters from given string.'''
+
+str = input("Enter string: ")
+trim_str = str.strip()
+print(f"String after trimming both ends: {trim_str}")
